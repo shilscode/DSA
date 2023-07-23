ce4f9d98c7554424043e78879af51c8ac761a868
shilscode <shils787@gmail.com>
2023-07-23 14:56:35 +0530

    3rd  commit

---
+++ 2nd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+ class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter ");
+        int numTerms = scanner.nextInt();
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < numTerms; i++) {
+            int fibonacciTerm = fibonacci(i);
+            System.out.print(fibonacciTerm + " ");
+        }
+
+        scanner.close();
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
