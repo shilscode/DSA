3066f9bc5d8ac3aa1e0fc4a25c175ca61374c502
shilscode <shils787@gmail.com>
2023-07-31 15:24:59 +0530

    new

---
+++ arr1.java
@@ -0,0 +1,21 @@
+
+
+
+public class arr1 {
+    public static void main (String[]args){
+
+        int arr[]= {12, 13, 14, 27 ,19,78};
+        for(int i=0; i<arr.length;i++){
+            if (arr[i]==14){
+                System.out.println("true");
+            }
+
+        }
+           
+                
+
+            
+
+    }
+    
+}
